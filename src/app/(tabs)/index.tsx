602cd0e376eb29c519af9317f92c3b45dfde634e
shabirkhan10 <135199362+shabirkhan10@users.noreply.github.com>
2024-10-29 15:56:11 +0500

    first commit

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,7 @@
+import { Text, View } from "react-native";
+
+export default function TabOneScreen() {
+  return <View className="flex-1 bg-red-500">
+    <Text className="text-white">Hello</Text>
+  </View>
+}
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,9 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Slot, Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import React, { useEffect } from 'react';
+import "./global.css";
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
